7f079191af0823b991379560be71cb02ab8cf751
headlessNode <harriskhan047@outlook.com>
2024-01-16 22:10:32 +0500

    Add test isSunk method returns true if hits equals length

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -2,20 +2,24 @@ import { expect, jest, test } from '@jest/globals';
 import Ship from '../modules/ship.js';
 
 describe('test ship methods', () => {
-  const shipLength = 3;
-  const shipObject = new Ship(shipLength);
   test('hit method is called', () => {
+    const shipLength = 3;
+    const shipObject = new Ship(shipLength);
     const hitSpy = jest.spyOn(shipObject, 'hit');
 
     expect(shipObject.hit()).toBeUndefined();
     expect(hitSpy).toHaveBeenCalled();
   });
   test('isSunk method is called', () => {
+    const shipLength = 3;
+    const shipObject = new Ship(shipLength);
     const isSunkSpy = jest.spyOn(shipObject, 'isSunk');
     expect(shipObject.isSunk()).toBe(false);
     expect(isSunkSpy).toHaveBeenCalled();
   });
   test('hit method throws error when call > length', () => {
+    const shipLength = 3;
+    const shipObject = new Ship(shipLength);
     const hitSpy = jest.spyOn(shipObject, 'hit');
     for (let i = 0; i === shipLength + 1; i++) {
       if (i > shipLength) {
@@ -26,5 +30,13 @@ describe('test ship methods', () => {
         expect(hitSpy).toHaveBeenCalled();
       }
     }
+  });
+  test('isSunk method returns true if hits equals length', () => {
+    const shipLength = 3;
+    const shipObject = new Ship(shipLength);
+    shipObject.hit();
+    shipObject.hit();
+    shipObject.hit();
+    expect(shipObject.isSunk()).toBe(true);
   });
 });
