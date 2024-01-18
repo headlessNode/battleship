c8e8c2f2a6716dd1e3943b5fdaa38bdd5e9abf5e
headlessNode <harriskhan047@outlook.com>
2024-01-18 14:28:06 +0500

    Change the gameBoard test to check Ship presence - change the private length property of Ship to public - remove position property from Ship - add 2D array as coordinates property for Gameboard - remove rows and columns property from Gameboard - add the test to check for ship presence in the position supplied to placeShip method - placeShip method places ship with horizontal orientation in the Gameboard coordinates

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,21 +1,19 @@
 class Ship {
-  #length;
   #nHits;
   constructor(length) {
-    this.#length = length;
+    this.length = length;
     this.#nHits = 0;
-    this.position;
     this.orientatoin;
   }
   hit() {
-    if (this.#nHits < this.#length) {
+    if (this.#nHits < this.length) {
       this.#nHits++;
     } else {
       throw new Error('Hits cannot exceed length');
     }
   }
   isSunk() {
-    if (this.#length === this.#nHits) {
+    if (this.length === this.#nHits) {
       return true;
     } else {
       return false;
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -5,9 +5,21 @@ import Ship from '../modules/ship.js';
 describe('test gameBoard methods', () => {
   const boardOne = new Gameboard();
   const shipObject = new Ship(4);
-  test('place ship at a specific position', () => {
-    boardOne.placeShip(shipObject, ['A', 4], 'horizontal');
-    expect(shipObject.position).toEqual(['A', 4]);
+  test('place ship at specific coordinates', () => {
+    const shipPosition = [3, 4];
+    const positionRow = shipPosition[0];
+    const positionCol = shipPosition[1];
+    const shipEndpoint = positionCol + shipObject.length;
+    boardOne.placeShip(shipObject, shipPosition, 'horizontal');
     expect(shipObject.orientation).toMatch('horizontal');
+    for (let i = 0; i < boardOne.coordinates.length; i++) {
+      for (let j = 0; j < boardOne.coordinates[i].length; j++) {
+        if (positionRow === i && positionCol === j) {
+          expect(boardOne.coordinates[i][j]).toEqual(1);
+        } else if (positionRow === i && j > positionCol && j <= shipEndpoint) {
+          expect(boardOne.coordinates[i][j]).toEqual(1);
+        }
+      }
+    }
   });
 });
