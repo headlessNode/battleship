3919c28d9762e8fede1b806c8b1ed88f2e968bec
headlessNode <harriskhan047@outlook.com>
2024-01-19 00:09:57 +0500

    Remove redundant code

--- src/modules/main.js
+++
@@ -1,7 +0,0 @@
-import Ship from './ship.js';
-import Gameboard from './gameBoard.js';
-
-const gameBoardOne = new Gameboard();
-
-const shipOne = gameBoardOne.placeShip(3, [2, 3], 'H');
-const shipTwo = gameBoardOne.placeShip(4, [3, 1], 'V');
