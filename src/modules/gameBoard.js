a83a068a60e3c51cba99b86aa44e12c779e2a09b
headlessNode <harriskhan047@outlook.com>
2024-01-18 15:26:10 +0500

    Add test for Ship with vertical orientation - add logic in placeShip method to account for ship with vertical orientation.

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -18,13 +18,23 @@ class Gameboard {
   //Gameboards should be able to place ships at specific coordinates by calling the ship factory function.
   placeShip(shipObject, position, orientation) {
     shipObject.orientation = orientation;
-    let positionRow = position[0];
-    let positionCol = position[1];
-    let shipEndpoint = positionCol + shipObject.length;
-    if (shipObject.orientation === 'horizontal') {
+    const positionRow = position[0];
+    const positionCol = position[1];
+
+    if (shipObject.orientation === 'H') {
+      const shipEndpoint = positionCol + shipObject.length;
       for (let i = 0; i < this.coordinates.length; i++) {
         for (let j = 0; j < this.coordinates[i].length; j++) {
           if (j >= positionCol && j <= shipEndpoint) {
+            this.coordinates[i][j] = 1;
+          }
+        }
+      }
+    } else if (shipObject.orientation === 'V') {
+      const shipEndpoint = positionRow + shipObject.length;
+      for (let i = 0; i < this.coordinates.length; i++) {
+        for (let j = 0; j < this.coordinates[i].length; j++) {
+          if (j === positionCol && i <= shipEndpoint) {
             this.coordinates[i][j] = 1;
           }
         }
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -5,18 +5,33 @@ import Ship from '../modules/ship.js';
 describe('test gameBoard methods', () => {
   const boardOne = new Gameboard();
   const shipObject = new Ship(4);
-  test('place ship at specific coordinates', () => {
+  test('place ship at specific coordinates with horizontal orientation', () => {
     const shipPosition = [3, 4];
     const positionRow = shipPosition[0];
     const positionCol = shipPosition[1];
     const shipEndpoint = positionCol + shipObject.length;
-    boardOne.placeShip(shipObject, shipPosition, 'horizontal');
-    expect(shipObject.orientation).toMatch('horizontal');
+    boardOne.placeShip(shipObject, shipPosition, 'H');
+    expect(shipObject.orientation).toMatch('H');
     for (let i = 0; i < boardOne.coordinates.length; i++) {
       for (let j = 0; j < boardOne.coordinates[i].length; j++) {
         if (positionRow === i && positionCol === j) {
           expect(boardOne.coordinates[i][j]).toEqual(1);
         } else if (positionRow === i && j > positionCol && j <= shipEndpoint) {
+          expect(boardOne.coordinates[i][j]).toEqual(1);
+        }
+      }
+    }
+  });
+  test('place ship at specific coordinates with vertical orientation', () => {
+    const shipPosition = [3, 4];
+    const positionRow = shipPosition[0];
+    const positionCol = shipPosition[1];
+    const shipEndpoint = positionRow + shipObject.length;
+    boardOne.placeShip(shipObject, shipPosition, 'V');
+    expect(shipObject.orientation).toMatch('V');
+    for (let i = 0; i < boardOne.coordinates.length; i++) {
+      for (let j = 0; j < boardOne.coordinates[i].length; j++) {
+        if (j === positionCol && i > positionRow && i <= shipEndpoint) {
           expect(boardOne.coordinates[i][j]).toEqual(1);
         }
       }
