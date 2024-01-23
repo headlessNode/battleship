0b370a96955cbf217dddd46692ff1c330e6f82f0
headlessNode <harriskhan047@outlook.com>
2024-01-23 12:36:33 +0500

    Add logic to prevent placing ships on adjacent rows and columns

--- src/modules/gameBoard.js
+++ src/modules/gameBoard.js
@@ -147,6 +147,46 @@ class Gameboard {
               }
             }
           }
+          let topAdjacentRow = positionRow - 1;
+          let topAdjacentCol = positionCol - 1;
+          let btmAdjacentRow = positionRow + 1;
+          let btmAdjacentCol = positionCol - 1;
+          if (
+            positionRow >= 0 &&
+            positionRow <= 9 &&
+            positionCol - 1 >= 0 &&
+            positionCol - 1 <= 9
+          ) {
+            this.coordinates[positionRow][positionCol - 1].ship = 'x';
+          }
+          if (
+            positionRow >= 0 &&
+            positionRow <= 9 &&
+            shipEndpoint + 1 >= 0 &&
+            shipEndpoint + 1 <= 9
+          ) {
+            this.coordinates[positionRow][shipEndpoint + 1].ship = 'x';
+          }
+          for (let i = 0; i <= shipObject.length + 2; i++) {
+            if (
+              topAdjacentRow >= 0 &&
+              topAdjacentRow <= 9 &&
+              topAdjacentCol >= 0 &&
+              topAdjacentCol <= 9
+            ) {
+              this.coordinates[topAdjacentRow][topAdjacentCol].ship = 'x';
+            }
+            if (
+              btmAdjacentRow >= 0 &&
+              btmAdjacentRow <= 9 &&
+              btmAdjacentCol >= 0 &&
+              btmAdjacentCol <= 9
+            ) {
+              this.coordinates[btmAdjacentRow][btmAdjacentCol].ship = 'x';
+            }
+            topAdjacentCol++;
+            btmAdjacentCol++;
+          }
         } else {
           throw new Error('Invalid Position: Out of bounds.');
         }
@@ -164,6 +204,46 @@ class Gameboard {
                 }
               }
             }
+          }
+          let leftAdjacentRow = positionRow - 1;
+          let leftAdjacentCol = positionCol - 1;
+          let rightAdjacentRow = positionRow + 1;
+          let rightAdjacentCol = positionCol + 1;
+          if (
+            positionRow - 1 >= 0 &&
+            positionRow - 1 <= 9 &&
+            positionCol >= 0 &&
+            positionCol <= 9
+          ) {
+            this.coordinates[positionRow - 1][positionCol].ship = 'x';
+          }
+          if (
+            shipEndpoint + 1 >= 0 &&
+            shipEndpoint + 1 <= 9 &&
+            positionCol >= 0 &&
+            positionCol <= 9
+          ) {
+            this.coordinates[shipEndpoint + 1][positionCol].ship = 'x';
+          }
+          for (let i = 0; i <= shipObject.length + 2; i++) {
+            if (
+              leftAdjacentRow >= 0 &&
+              leftAdjacentRow <= 9 &&
+              leftAdjacentCol >= 0 &&
+              leftAdjacentCol <= 9
+            ) {
+              this.coordinates[leftAdjacentRow][leftAdjacentCol].ship = 'x';
+            }
+            if (
+              rightAdjacentRow >= 0 &&
+              rightAdjacentRow <= 9 &&
+              rightAdjacentCol >= 0 &&
+              rightAdjacentCol <= 9
+            ) {
+              this.coordinates[rightAdjacentRow][rightAdjacentCol].ship = 'x';
+            }
+            leftAdjacentRow++;
+            rightAdjacentRow++;
           }
         } else {
           throw new Error('Invalid Position: Out of bounds.');
